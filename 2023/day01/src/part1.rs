a7bb13d274ad6e0e209cdeddfcad9db332da18ca
arustydev <openwrt-ios@rusty.aaln.one>
2023-12-01 14:48:11 -0500

    Part1: Complete!!

--- 2023/day01/src/part1.rs
+++ 2023/day01/src/part1.rs
@@ -4,14 +4,14 @@ use regex::Regex;
 fn main() {
     // static NUMBERS: Vec::<String> = Vec::<String>::new();
     const FILE_NAME: &str = "input/part1.txt";
-    find_the_numbers(FILE_NAME);
+    println!("The sum of the numbers is: {}", find_the_numbers(FILE_NAME).into_iter().sum::<i32>());
 }
 
-fn find_the_numbers(file_path: &str) -> Vec<String> {
+fn find_the_numbers(file_path: &str) -> Vec<i32> {
     read_to_string(file_path) 
         .unwrap()
         .lines()
-        .map(|s| vec![firstnum(s.to_string()), firstnum(s.chars().rev().collect::<String>())].join(""))
+        .map(|s| vec![firstnum(s.to_string()), firstnum(s.chars().rev().collect::<String>())].join("").parse::<i32>().unwrap())
         .collect()
 }
 
@@ -25,7 +25,17 @@ mod tests {
     use super::*;
 
     #[test]
-    fn check_firstnum() {
+    #[cfg_attr(not(feature = "part1"), ignore)]
+    fn find_the_numbers_test() {
+        const FILE_NAME: &str = "input/part1_test.txt";
+        let numvec: Vec<i32> = find_the_numbers(FILE_NAME);
+        assert_eq!(vec![12, 38, 15, 77], numvec);
+        assert_eq!(142, numvec.into_iter().sum());
+    }
+
+    #[test]
+    #[cfg_attr(not(feature = "part1"), ignore)]
+    fn firstnum_test() {
         let numstr: String = "Ru5t4Fun".to_string();
         let rtsmun: String = numstr.chars().rev().collect::<String>();
         assert_eq!("5".to_string(),firstnum(numstr));
