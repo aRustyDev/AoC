00b012cdbddc6e69866bb17cfc8d9b1502127863
arustydev <openwrt-ios@rusty.aaln.one>
2023-12-01 10:00:04 -0500

    impl test 'file_opens', impl file reading in fn

--- 2023/day01/src/part1.rs
+++ 2023/day01/src/part1.rs
@@ -1,11 +1,23 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    find_the_numbers("input/part1.txt");
+}
+
+fn find_the_numbers(file_path) {
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn exploration() {
+    fn file_opens() {
+        find_the_numbers("input/part1.txt");
         assert_eq!(2 + 2, 4);
     }
 }
