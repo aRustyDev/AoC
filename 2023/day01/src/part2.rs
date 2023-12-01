a003fa4089b8afdbc1ad59af6cd905b4ce70f455
arustydev <openwrt-ios@rusty.aaln.one>
2023-12-01 14:50:54 -0500

    Part2: Starts here

---
+++ 2023/day01/src/part2.rs
@@ -0,0 +1,17 @@
+
+fn main() {
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[cfg_attr(not(feature = "part2"), ignore)]
+    fn fn_one_test() {
+        // stuff here
+        // assert_eq!(142, numvec);
+    }
+}
